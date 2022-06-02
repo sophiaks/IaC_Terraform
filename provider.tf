e0ddc7328ce2ffa892bbc5688edb490f7f21146a
Sophia K. S <sophia.shigueoka@gmail.com>
2022-06-01 21:12:55 -0300

    Starting from scratch with modules

--- provider.tf
+++
@@ -1,5 +0,0 @@
-provider "aws" {
-    region = "${var.AWS_REGION}"
-    // shared_config_files = ["~/.aws/config"]
-    shared_credentials_files = ["~/.aws/credentials"]
-}
--- vars.tf
+++
@@ -1,8 +0,0 @@
-variable "AWS_REGION" {
-    default = "us-east-1"
-}
-
-variable "cluster-name" {
-  default = "eks-cluster"
-  type    = string
-}
