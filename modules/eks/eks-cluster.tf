fbce8dc6b203a835916ef851a0e69b94dfc1ea63
Sophia K. S <sophia.shigueoka@gmail.com>
2022-06-02 22:11:55 -0300

    Creating infrastructure using modules

--- modules/eks/eks-cluster.tf
+++ modules/eks/eks-cluster.tf
@@ -1,36 +1,3 @@
-## VPC ##
-module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "sopas-vpc"
-  cidr = "10.0.0.0/16"
-
-  azs             = "${var.azs}"
-  private_subnets = "${var.private_subnets}"
-  public_subnets  = "${var.public_subnets}"
-
-  enable_nat_gateway = true
-  single_nat_gateway  = false
-  reuse_nat_ips       = true
-  external_nat_ip_ids = "${aws_eip.nat.*.id}"
-
-  enable_vpn_gateway = true
-  
-  tags = {
-    Terraform = "true"
-    Environment = "dev"
-  }
-}
-
-## NAT ##
-
-resource "aws_eip" "nat" {
-  count = 3
-  vpc = true
-}
-
-## EKS ##
-
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 18.0"
@@ -56,10 +23,8 @@ module "eks" {
     resources        = ["secrets"]
   }]
 
-  ################ AAAAAAAAAAAAAAAAA
-  ##################AAAAAAAAAAAA
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = ["id1", "id2"]
+  vpc_id     = "vpc-1234556abcdef"
+  subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
 
   # Self Managed Node Group(s)
   self_managed_node_group_defaults = {
@@ -73,7 +38,7 @@ module "eks" {
   self_managed_node_groups = {
     one = {
       name         = "mixed-1"
-      max_size     = 3
+      max_size     = 5
       desired_size = 2
 
       use_mixed_instances_policy = true
@@ -86,11 +51,11 @@ module "eks" {
 
         override = [
           {
-            instance_type     = "m2.micro"
+            instance_type     = "m5.large"
             weighted_capacity = "1"
           },
           {
-            instance_type     = "m2.micro"
+            instance_type     = "m6i.large"
             weighted_capacity = "2"
           },
         ]
@@ -162,76 +127,3 @@ module "eks" {
     Terraform   = "true"
   }
 }
-
-## SECURITY GROUP ##
-
-module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = "vpc-123"
-
-  ingress_cidr_blocks      = ["10.10.0.0/16"]
-  ingress_rules            = ["https-443-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
-}
-
-## IAM ROLE ##
-
-module "iam_assumable_role" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role"
-
-  trusted_role_arns = [
-    "arn:aws:iam::307990089504:root",
-    "arn:aws:iam::835367859851:user/anton",
-  ]
-
-  create_role = true
-
-  role_name         = "custom"
-  role_requires_mfa = true
-
-  custom_role_policy_arns = [
-    "arn:aws:iam::aws:policy/AmazonCognitoReadOnly",
-    "arn:aws:iam::aws:policy/AlexaForBusinessFullAccess",
-  ]
-  number_of_custom_role_policy_arns = 2
-}
-
-
-## EC2 Instance ##
-
-module "ec2_instance" {
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  version = "~> 3.0"
-
-  for_each = toset(["django1", "django2", "postgresql"])
-
-  name = "instance-${each.key}"
-
-  ami                    = "${var.ami_ubuntu}"
-  instance_type          = "t2.micro"
-  key_name               = "user1"
-  monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
-  subnet_id              = "subnet-eddcdzz4"
-
-  tags = {
-    Terraform   = "true"
-    Environment = "dev"
-  }
-}
-
