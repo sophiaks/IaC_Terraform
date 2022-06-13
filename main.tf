f4aae4f39ac04da0213e557ea6cda03b726de360
Sophia Kerber <sophia.shigueoka@gmail.com>
2022-06-13 05:44:11 -0300

    Adding eks module to project

--- main.tf
+++ main.tf
@@ -1,16 +1,22 @@
-module "sopa_vpc" {
-    source = "./modules/vpc"
-    vpc_cidr = "10.0.0.0/16"
-    vpc_id = "${aws_vpc.main_vpc.id}"
-    tenancy = "default"
-    public_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-    private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
-    availability_zones = ["us-east-1", "us-west-1"]
+module "vpc" {
+  source = "./modules/vpc"
+  vpc_cidr = "10.0.0.0/16"
+  vpc_id = "${aws_vpc.main_vpc.id}"
+  tenancy = "default"
+  public_subnets_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets_cidr = ["10.0.3.0/24", "10.0.4.0/24"]
+  availability_zones = ["us-east-1a", "us-east-1b"]
+  enable_nat_gateway   = true
 }
 
-module "sopa_ec2" {
-    source = "./modules/ec2"
-    ec2_count = 2
-    instance_type = "t2.micro"
-    subnet_id = "${aws_subnet.public_subnet.*.id}"
+module "eks" {
+  source = "./modules/eks"
+  version = "~> 18.0"
+
+  cluster_name    = "sopas-cluster"
+  cluster_version = "1.21"
+
+  vpc_id     = modules.vpc.id
+  // TODO: Add subnets
+  subnet_ids = ["subnet-abcde012", "subnet-bcde012a", "subnet-fghi345a"]
 }
